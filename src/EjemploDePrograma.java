03c9bc8ea06fca525a60b4cafc12180f0880fa91
juanmi-devv <tabletsanchez3@gmail.com>
2025-12-23 02:48:07 +0100

    Hola mundo

---
+++ src/EjemploDePrograma.java
@@ -0,0 +1,6 @@
+public class EjemploDePrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hola inés te demuestro como se programa en Java");
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    // la primera clase siempre tiene que tener el nombre del fichero.
+
+    // si dentro de la clase no existe la clase public static void main, no se va a ejecutar el programa.
+    // cuando se ejecute, solamente se va a ejecutar la segunda clase.
+    // public significa que se ve en todo el proyecto.
+
+    public static void main(String[] args) {    // Esto es un metodo main. void se usa para ejecutar el codigo pero no devuelve nada.
+        System.out.println("Mi primer hola mundo en JAVA"); // println lo que hace es un salto de linea cada vez que imprime. system es un paquete de sistema de java. out es salida estandard del sistema.
+
+    }
+}
+
+// Comentrios en una linea.
+
+/*
+Comentarios
+En
+Diferentes lineas.
+*/
